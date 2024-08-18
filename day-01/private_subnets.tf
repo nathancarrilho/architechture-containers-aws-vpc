e01140629f878d3525140c2d557698fcbef92b12
nathancarrilho <nathancarrilholeal@gmail.com>
2024-08-18 18:30:13 +0100

    update 002

--- day-01/private_subnets.tf
+++ day-01/private_subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "private_subnet_1a" {
   vpc_id = aws_vpc.main.id
   
   cidr_block = "10.0.0.0/20"
-  availability_zone = format("%s", var.region)
+  availability_zone = format("%sa", var.region)
 
   tags = {
     Name = format("%s-private-subnet-1a", var.project_name )
@@ -13,7 +13,7 @@ resource "aws_subnet" "private_subnet_1b" {
   vpc_id = aws_vpc.main.id
   
   cidr_block = "10.0.16.0/20"
-  availability_zone = format("%s", var.region)
+  availability_zone = format("%sb", var.region)
 
   tags = {
     Name = format("%s-private-subnet-1b", var.project_name )
@@ -24,7 +24,7 @@ resource "aws_subnet" "private_subnet_1c" {
   vpc_id = aws_vpc.main.id
   
   cidr_block = "10.0.32.0/20"
-  availability_zone = format("%s", var.region)
+  availability_zone = format("%sc", var.region)
 
   tags = {
     Name = format("%s-private-subnet-1c", var.project_name )
--- day-01/public_subnets.tf
+++ day-01/public_subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "public_subnet_1a" {
   vpc_id = aws_vpc.main.id
   
   cidr_block = "10.0.48.0/24"
-  availability_zone = format("%s", var.region)
+  availability_zone = format("%sa", var.region)
 
   tags = {
     Name = format("%s-public-subnete-1a", var.project_name )
@@ -13,7 +13,7 @@ resource "aws_subnet" "public_subnet_1b" {
   vpc_id = aws_vpc.main.id
   
   cidr_block = "10.0.49.0/24"
-  availability_zone = format("%s", var.region)
+  availability_zone = format("%sb", var.region)
 
   tags = {
     Name = format("%s-public-subnete-1b", var.project_name )
@@ -24,7 +24,7 @@ resource "aws_subnet" "public_subnet_1c" {
     vpc_id = aws_vpc.main.id
   
     cidr_block = "10.0.50.0/24"
-    availability_zone = format("%s", var.region)
+    availability_zone = format("%sc", var.region)
 
     tags = {
       Name = format("%s-public-subnete-1c", var.project_name )
